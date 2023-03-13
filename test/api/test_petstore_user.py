f10eadd20bc419f6bf0bd6950df280b9ff920cf4
ceara25 <vmsmantovani@gmail.com>
2023-03-13 16:02:45 -0300

    Criação do definição de teste do login e passagem do token para a consulta.

--- test/api/test_petstore_user.py
+++ test/api/test_petstore_user.py
@@ -1,10 +1,13 @@
 # Bibliotecas
+import time
+
 import pytest  # Framework de Teste Unitário - Engine
 import requests  # Framework de Teste de API - Requests / Responses
 
 # Endereço da API
 base_url = 'https://petstore.swagger.io/v2/user'
 headers = {'Content-Type': 'aplication/json'}
+token_usuario = ''
 
 
 # Os testes
@@ -49,7 +52,6 @@ def testar_consultar_usuario():
 
     # Executa
     resposta = requests.get(
-        #
         url=f'{base_url}/{username}',
         headers=headers
     )
@@ -66,6 +68,41 @@ def testar_consultar_usuario():
     assert corpo_da_resposta['username'] == username
     assert corpo_da_resposta['email'] == email
     assert corpo_da_resposta['phone'] == phone
+
+    print(f'Token: {token_usuario}')
+
+def testar_consultar_usuario_com_token(token_usuario):
+    # Configura
+    status_code = 200
+    id = 1008
+    username = 'stevemaster'
+    firstName = 'steve'
+    lastName = 'stifler'
+    email = 'steve.stifler@teste.com'
+    password = '123456'
+    phone = '11986390745'
+    userStatus = 0
+
+    # Executa
+    resposta = requests.get(
+        url=f'{base_url}/{username}',
+        headers=headers
+    )
+
+    # Formatação
+    corpo_da_resposta = resposta.json()  # Formata como JSON
+    print(resposta)  # Resposta Bruta
+    print(resposta.status_code)  # Status Code
+    print(corpo_da_resposta)  # Resposta Formatada
+
+    # Valida
+    assert resposta.status_code == status_code
+    assert corpo_da_resposta['id'] == id
+    assert corpo_da_resposta['username'] == username
+    assert corpo_da_resposta['email'] == email
+    assert corpo_da_resposta['phone'] == phone
+
+    print(f'Token: {token_usuario}')
 
 
 def testar_alterar_usuario():
@@ -130,5 +167,58 @@ def testar_excluir_usuario():
 
         case 404:
             print('usuário não encontrado!')
+
+def testar_login_do_usuario():
+    # Configura
+    username = 'stevemaster'
+    password = '123456'
+    status_code_esperado = 200
+    codigo_esperado = 200
+    tipo_esperado = 'unknown'
+    inicio_mensagem_esperada = 'logged in user session:'
+
+    # Executa
+    resposta = requests.get(
+        url=f'{base_url}/login?username={username}&password={password}',
+        headers=headers
+    )
+
+    # Formata
+    corpo_da_resposta = resposta.json()
+    print(resposta)
+    print(resposta.status_code)
+    print(corpo_da_resposta)
+
+    # Valida
+    assert resposta.status_code == status_code_esperado
+    assert corpo_da_resposta['code'] == codigo_esperado
+    assert corpo_da_resposta['type'] == tipo_esperado
+    assert corpo_da_resposta['message'].find(inicio_mensagem_esperada) != -1
+
+    frase = 'Neste fim de ano planeje o seu sucesso'
+    assert frase.find('sucesso') != -1
+
+    # Extrair
+    # Na mensagem "logged in user session:1678713953093" queremos pegar os números
+    mensagem_recebida = corpo_da_resposta['message']
+    token_usuario = mensagem_recebida[23:37]
+    print(f'O token do usuario é: {token_usuario}')
+    testar_consultar_usuario_com_token(token_usuario)
+
+    # Exemplo
+    frase = 'Saldo: R$ 1.987,65'
+    valor = frase[7:18]
+    print(f'O valor é: {valor}')
+
+'''
+def testar_sequencia_de_testes():
+    testar_criar_usuario()
+    testar_login_do_usuario()
+    testar_alterar_usuario()
+    testar_consultar_usuario()
+    testar_excluir_usuario()
+'''
+
+
 
 
